58969b0fe9e92c290bbb2c5b79c1bd90f426267d
AlpBuz <alpbuz@buffalo.edu>
2024-10-03 18:40:01 -0400

    added the nosniff headers to all responses, also changed the port to 8080 because it has to be in that

--- run.py
+++ run.py
@@ -2,5 +2,15 @@ from app import create_app
 
 app = create_app()
 
+@app.after_request
+def nosniffSecurity(response):
+    """
+    This will add the noSniff header to all response right before it gets sent
+    """
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
+
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
