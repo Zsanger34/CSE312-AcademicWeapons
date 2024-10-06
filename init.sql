467b193be294bc2ee4c9d061a576df7a9a091bda
AlpBuz <alpbuz@buffalo.edu>
2024-10-05 22:08:48 -0400

    deleted test statements

--- init.sql
+++ init.sql
@@ -5,22 +5,3 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL, -- Password column
     cookie TEXT                     -- Cookie column
 );
-
-
---Test to see if it worked
-INSERT INTO users (username, password)
-VALUES ('TestUser1', 'my_password123!');
---DELETE THIS TEST BEFORE SUBMITTING TO AUTOLAB
-
---Test to see if it worked
-INSERT INTO users (username, password)
-VALUES ('TestUser2', 'my_password123456789!');
---DELETE THIS TEST BEFORE SUBMITTING TO AUTOLAB
-
---Test to see if another table is added
-CREATE TABLE IF NOT EXISTS Test (
-    id SERIAL PRIMARY KEY,
-    Testing VARCHAR(100) NOT NULL,
-    Testcolumn VARCHAR(255) NOT NULL,
-    cookie TEXT
-);
