54a4e2970f2b6c2b3d8a76805db6318af29052bc
n1bby <njbousqu@buffalo.edu>
2024-10-06 12:42:38 -0400

    so homepage redirects to the register page if their is no cookie token. and also displays the usernames name

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
 import psycopg2
 
 # Blueprint allows you to organize routes
@@ -17,5 +17,20 @@ def get_db_connection():
 # Define the home route
 @main_routes.route('/')
 def home():
-    return render_template('index.html')
+    
+    session_token = request.cookies.get('session_token')
+    if not session_token:
+        return redirect(url_for('register.register'))
+    # Query the database for the user
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT username FROM users WHERE cookie = %s', (session_token,))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if user:
+        # Pass the username to the template
+        return render_template('index.html', username=user[0])
+    else:
+        return redirect(url_for('register.register'))
 
