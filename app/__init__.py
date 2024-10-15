64cd3a4a9bdfb4a2c8e3808b259d5adceeba2653
AlpBuz <alpbuz@buffalo.edu>
2024-10-15 16:50:50 -0400

    testing my commit?

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .routes import main_routes
+from .settings import settings_routes
 
 def create_app():
     app = Flask(__name__)
     
     # Register Blueprints
     app.register_blueprint(main_routes)
+    app.register_blueprint(settings_routes)
 
     return app
