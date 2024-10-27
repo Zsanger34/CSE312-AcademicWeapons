f423e93c8f6c80676f73e2b4267c05b08a9bce34
Zac Sanger <58315356+Zsanger34@users.noreply.github.com>
2024-10-27 11:50:57 -0400

    Fixed Routes Issue with Hashing Token if Token doesnt exist and Removed a print statemetn causes errors Code working currently

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, request, jsonify, render_template, redirect, url_for, escape, make_response
 import psycopg2
 import hashlib
@@ -6,44 +7,41 @@ import hashlib
 main_routes = Blueprint('main', __name__)
 
 # Database connection settings
-DB_HOST = 'db'
+DB_HOST = 'db'  
 DB_NAME = 'mydatabase'
 DB_USER = 'postgres'
 DB_PASSWORD = 'mysecretpassword'
-
 
 def get_db_connection():
     conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
     return conn
 
-
 # Define the home route
 @main_routes.route('/', methods=['GET', 'POST'])
 def home():
-    # get the cookie if the user is logged in
-    # the token is going to be plain text
+    
+    #get the cookie if the user is logged in
+    #the token is going to be plain text
     session_token = request.cookies.get('session_token')
-    hashed_token = hashlib.sha256(session_token.encode()).hexdigest()
     if not session_token:
-        return redirect(url_for('register.register'))
+        return redirect(url_for('login_page.login'))
+    else: 
+        hashed_token = hashlib.sha256(session_token.encode()).hexdigest()
 
-    # check to see if we got a post request.
+    #check to see if we got a post request.
     if request.method == "POST":
-        # the user is logged out and now we clear the cookie token
+        #the user is logged out and now we clear the cookie token
         response = make_response(jsonify({'message': 'Registration successful'}))
         response.set_cookie('session_token', session_token, httponly=True, secure=True, max_age=-3600)
         return response
 
     # Query the database for the user if user is logged in get their information
-    # hash the token taken from the session_token above code to look up the users info
-    # hashed_token = hashlib.sha256(session_token.encode()).hexdigest()
+    #hash the token taken from the session_token above code to look up the users info
+    #hashed_token = hashlib.sha256(session_token.encode()).hexdigest()
     conn = get_db_connection()
     cursor = conn.cursor()
-    hashed_token = hashlib.sha256(session_token.encode()).hexdigest()
     cursor.execute('SELECT username FROM users WHERE cookie = %s', (hashed_token,))
     user = cursor.fetchone()
-    if user is None:
-        return redirect(url_for('register.register'))
     cursor.close()
     conn.close()
     if user:
