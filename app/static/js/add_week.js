d71ca3f6c2a76bbdd93ef765511715448c458d76
Nathan Bousquet <njbousqu@buffalo.edu>
2024-11-30 10:31:59 -0500

    added the files to create a workout card to the dev

--- app/static/js/add_week.js
+++ app/static/js/add_week.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
             removeDay(day);
         });
         showDaydiv.appendChild(dayCard);
-    };//end render card 
+    };//end render card
 
     
     const removeDay = async (day) => {
